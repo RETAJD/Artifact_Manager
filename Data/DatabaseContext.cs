06721274b0263300d9532024f7a7be46f5e8c782
RETAJD <45774493+RETAJD@users.noreply.github.com>
2022-05-19 18:33:41 +0200

    Working project

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace App.Data
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base (@"Data Source=DESKTOP-9K2SBT4\SQLEXPRESS;Initial Catalog=DATABASE_APP;User ID=DESKTOP-9K2SBT4\admin;Password=;Integrated Security=True")
+        {
+        }
+
+        public virtual DbSet<Users> Userss { get; set; }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace App.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<App.Data.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "App.Data.DatabaseContext";
+        }
+
+        protected override void Seed(App.Data.DatabaseContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
